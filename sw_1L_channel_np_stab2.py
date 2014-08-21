e406ec64d6e2fd13d3752cc51edd40c5cb6c9e46
kimyousee <kusi@uwaterloo.ca>
2014-08-21 09:27:55 -0400

    Update sw_1L_channel_np_stab2.py

--- sw_1L_channel_np_stab2.py
+++ sw_1L_channel_np_stab2.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import scipy.linalg as spalg
 from scipy.sparse.linalg import eigs
 
-## Code for using nump/scipy only (with eig or eigs)
+## Code for using numpy/scipy only (with eig or eigs)
 
 opts = PETSc.Options()
 nEV = opts.getInt('nev', 5)
@@ -30,23 +30,26 @@ y = np.linspace(0,Ly, Ny+1)
 hy = y[1] - y[0]
 e = np.ones(Ny+1)
 
-Dy = sp.spdiags([-1*e, 0*e, e]/(2*hy), [-1, 0, 1], Ny+1,Ny+1).todense()
+Dy = sp.spdiags([-1*e, 0*e, e]/(2*hy), [-1, 0, 1], Ny+1,Ny+1)
+Dy = sp.csr_matrix(Dy)
+# Dy = sp.lil_matrix(Dy)
 Dy[0, 0:2] = [-1, 1]/hy
 Dy[Ny, Ny-1:Ny+1] = [-1, 1]/hy
-sp.dia_matrix(Dy)
 
-Dy2 = sp.spdiags([e, -2*e, e]/hy**2, [-1, 0, 1], Ny+1, Ny+1).todense()
+Dy2 = sp.spdiags([e, -2*e, e]/hy**2, [-1, 0, 1], Ny+1, Ny+1)
+# Dy2 = sp.lil_matrix(Dy2)
+Dy2 = sp.csr_matrix(Dy2)
 Dy2[0, 0:3] = [1, -2, 1]/hy**2
 Dy2[Ny, Ny-2:Ny+1] = [1,-2,1]/hy**2
-sp.dia_matrix(Dy2)
 
 Eta = -0.1*np.tanh((y-Ly/2)/Lj)
 H = Eta + Hm
-U = -g0/f0*np.dot(Dy,Eta)
+U = -g0/f0*Dy*Eta
 U = np.transpose(U)
-dU = np.dot(Dy,U)
-dH = np.dot(Dy,H)
+dU = Dy*U
+dH = Dy*H
 H1 = sp.spdiags(H, 0, Ny+1,Ny+1)
+H1 = sp.csr_matrix(H1)
 HDy = H1*Dy
 
 kk = np.arange(dkk,2+dkk,dkk)/Lj
@@ -59,81 +62,82 @@ mode = np.zeros([3*Ny+1,nEV,nk], dtype=complex)
 # frOut = open('freq.dat', 'wb+')
 # mdOut = open('mode.dat', 'wb+')
 
-A1 = np.zeros([3*Ny+1,3*Ny+1])
+# A1 = np.zeros([3*Ny+1,3*Ny+1])
+A1 = sp.csr_matrix((3*Ny+1,3*Ny+1))
 
-A1[0,0]   = U[0]	## Block A00 corners
-A1[Ny,Ny] = U[Ny]	##
+A1[0,0]   = U[0]    ## Block A00 corners
+A1[Ny,Ny] = U[Ny]   ##
 
-A1[0, 2*Ny] = g0 	## Block A02 corners
+A1[0, 2*Ny] = g0    ## Block A02 corners
 A1[Ny,3*Ny] = g0 
 
-A1[2*Ny,0] = H[0]	## Block A20 corners
+A1[2*Ny,0] = H[0]   ## Block A20 corners
 A1[3*Ny,Ny] = H[Ny]
 
 A1[2*Ny,2*Ny] = U[0]## Block A22 corners
 A1[3*Ny,3*Ny] = U[Ny]
 
 # Block A21 top 2 and bottom 2 corners
-A1[2*Ny,   Ny+1] = 			HDy[0,1]
-A1[2*Ny+1, Ny+1:Ny+3] = 	[dH[0,1],0] + HDy[1,1:3]
-A1[3*Ny-1, 2*Ny-2:2*Ny] = 	[0,dH[0,Ny-1]] + HDy[Ny-1,Ny-2:Ny]
-A1[3*Ny,   2*Ny-1] = 		HDy[Ny,Ny-1]
+A1[2*Ny,   Ny+1] =          HDy[0,1]
+A1[2*Ny+1, Ny+1:Ny+3] =     [dH[1],0] + HDy[1,1:3].todense()
+A1[3*Ny-1, 2*Ny-2:2*Ny] =   [0,dH[Ny-1]] + HDy[Ny-1,Ny-2:Ny].todense()
+A1[3*Ny,   2*Ny-1] =        HDy[Ny,Ny-1]
 
-for ii in xrange(1,Ny):
-	A1[ii,ii]      = U[ii]
-	A1[ii,Ny+ii]   = dU[ii] - f0
-	A1[ii,2*Ny+ii] = g0
+for ii in range(1,Ny):
+    A1[ii,ii]      = U[ii]
+    A1[ii,Ny+ii]   = dU[ii] - f0
+    A1[ii,2*Ny+ii] = g0
 
-	A1[ii+Ny,ii+Ny] = U[ii]
+    A1[ii+Ny,ii+Ny] = U[ii]
 
-	A1[ii+2*Ny,ii]  = H[ii]
-	A1[ii+2*Ny,2*Ny+ii] = U[ii]
+    A1[ii+2*Ny,ii]  = H[ii]
+    A1[ii+2*Ny,2*Ny+ii] = U[ii]
 
-	if ii == 1 or ii == (Ny-1):
-		continue
-	A1[ii+2*Ny, Ny+ii-1:Ny+ii+2] = [0,dH[0,ii],0] + HDy[ii,ii-1:ii+2]
-sp.dia_matrix(A1)
+    if ii == 1 or ii == (Ny-1):
+        continue
+    A1[ii+2*Ny, Ny+ii-1:Ny+ii+2] = [0,dH[ii],0] + HDy[ii,ii-1:ii+2].todense()
 
-A2 = np.zeros([3*Ny+1, 3*Ny+1])
-for ii in xrange(1,Ny):
-	A2[Ny+ii,ii] = -f0
-	A2[Ny+ii,2*Ny+ii-1:2*Ny+ii+2] = -g0*Dy[ii,ii-1:ii+2]
-sp.dia_matrix(A2)
 
-evecOut = open('eigVec.bin', 'wb+')
-evalOut = open('eigVal.bin', 'wb+')
+# A2 = np.zeros([3*Ny+1, 3*Ny+1])
+A2 = sp.csr_matrix((3*Ny+1, 3*Ny+1))
+for ii in range(1,Ny):
+    A2[Ny+ii,ii] = -f0
+    A2[Ny+ii,2*Ny+ii-1:2*Ny+ii+2] = -g0*Dy[ii,ii-1:ii+2]
+
+# evecOut = open('eigVec.bin', 'wb+')
+# evalOut = open('eigVal.bin', 'wb+')
 
 cnt = 0
 guess=0.21+0.09*1j
 
-for kx in kk[0:nk]: #0:91... might not converge near the end
+for kx in kk[0:nk]: #0:nk
 
-	k2 = kx**2
+    k2 = kx**2
 
-	A = A1+A2/k2
-	sp.dia_matrix(A)
+    A = A1+A2/k2
+    # sp.dia_matrix(A)
 
-	# # Using eig
-	# eigVals, eigVecs = spalg.eig(A)
-	# ind = (-np.imag(eigVals)).argsort() #get indices in descending order
-	# eigVecs = eigVecs[:,ind]
-	# eigVals = kx*eigVals[ind]
-	# #print eigVals[:5]/kx
+    # # Using eig
+    # eigVals, eigVecs = spalg.eig(A.todense())
+    # ind = (-np.imag(eigVals)).argsort() #get indices in descending order
+    # eigVecs = eigVecs[:,ind]
+    # eigVals = kx*eigVals[ind]
+    
+    # #print eigVals[:5]/kx
 
-	# Using eigs
-	evals_all, evecs_all = eigs(A,nEV,ncv=10,which='LI',sigma=guess,maxiter=5000)
-	#print kx*evals_all[:]
-	evals_all.tofile(evalOut)
-	evecs_all.tofile(evecOut)
-	print evecs_all.shape,evals_all.shape
-	print evals_all[:]
-	if len(evals_all)>nEV: evals=nEV
-	else: evals = len(evals_all)
-	for i in xrange(evals):
-		grow[i,cnt] = np.imag(evals_all[i])*kx
-		freq[i,cnt] = np.real(evals_all[i])*kx
-		plt.plot(np.arange(0,nk),grow[i,:]*3600*24, 'o')
-	cnt = cnt+1
+    # Using eigs
+    evals_all, evecs_all = eigs(A,nEV,ncv=10,which='LI',sigma=guess,maxiter=5000)
+
+    # evals_all.tofile(evalOut)
+    # evecs_all.tofile(evecOut)
+
+    if len(evals_all)>nEV: evals=nEV
+    else: evals = len(evals_all)
+    for i in range(evals):
+        grow[i,cnt] = np.imag(evals_all[i])*kx
+        freq[i,cnt] = np.real(evals_all[i])*kx
+        plt.plot(np.arange(0,nk),grow[i,:]*3600*24, 'o')
+    cnt = cnt+1
 
 
 ky = np.pi/Ly
@@ -141,3 +145,4 @@ plt.ylabel('1/day')
 plt.xlabel('k/dk')
 plt.title('Growth Rate: 1-Layer SW2')
 plt.savefig('Grow1L_SW.eps', format='eps', dpi=1000)
+
