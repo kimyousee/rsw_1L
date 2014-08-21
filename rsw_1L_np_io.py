6fb32acaa59f46af9db5a819f5b0eb858f350e1e
kimyousee <kusi@uwaterloo.ca>
2014-08-21 09:44:13 -0400

    Update rsw_1L_np_io.py

--- rsw_1L_np_io.py
+++ rsw_1L_np_io.py
@@ -33,23 +33,27 @@ y = np.linspace(0,Ly, Ny+1)
 hy = y[1] - y[0]
 e = np.ones(Ny+1)
 
-Dy = sp.spdiags([-1*e, 0*e, e]/(2*hy), [-1, 0, 1], Ny+1,Ny+1).todense()
+Dy = sp.spdiags([-1*e, 0*e, e]/(2*hy), [-1, 0, 1], Ny+1,Ny+1)
+Dy = sp.csr_matrix(Dy)
+# Dy = sp.lil_matrix(Dy)
 Dy[0, 0:2] = [-1, 1]/hy
 Dy[Ny, Ny-1:Ny+1] = [-1, 1]/hy
-sp.dia_matrix(Dy)
 
-Dy2 = sp.spdiags([e, -2*e, e]/hy**2, [-1, 0, 1], Ny+1, Ny+1).todense()
+Dy2 = sp.spdiags([e, -2*e, e]/hy**2, [-1, 0, 1], Ny+1, Ny+1)
+# Dy2 = sp.lil_matrix(Dy2)
+Dy2 = sp.csr_matrix(Dy2)
 Dy2[0, 0:3] = [1, -2, 1]/hy**2
 Dy2[Ny, Ny-2:Ny+1] = [1,-2,1]/hy**2
-sp.dia_matrix(Dy2)
 
 Eta = -0.1*np.tanh((y-Ly/2)/Lj)
 H = Eta + Hm
-U = -g0/f0*np.dot(Dy,Eta)
+U = -g0/f0*Dy*Eta
 U = np.transpose(U)
-dU = np.dot(Dy,U)
-dH = np.dot(Dy,H)
+
+dU = Dy*U
+dH = Dy*H
 H1 = sp.spdiags(H, 0, Ny+1,Ny+1)
+H1 = sp.csr_matrix(H1)
 HDy = H1*Dy
 
 kk = np.arange(dkk,2+dkk,dkk)/Lj
@@ -62,7 +66,9 @@ grOut = open('storage/grow', 'wb+')
 frOut = open('storage/freq', 'wb+')
 mdOut = open('storage/mode', 'wb+')
 
-A1 = np.zeros([3*Ny+1,3*Ny+1])
+
+# A1 = np.zeros([3*Ny+1,3*Ny+1])
+A1 = sp.csr_matrix((3*Ny+1,3*Ny+1))
 
 A1[0,0]   = U[0]    ## Block A00 corners
 A1[Ny,Ny] = U[Ny]   ##
@@ -78,11 +84,11 @@ A1[3*Ny,3*Ny] = U[Ny]
 
 # Block A21 top 2 and bottom 2 corners
 A1[2*Ny,   Ny+1] =          HDy[0,1]
-A1[2*Ny+1, Ny+1:Ny+3] =     [dH[0,1],0] + HDy[1,1:3]
-A1[3*Ny-1, 2*Ny-2:2*Ny] =   [0,dH[0,Ny-1]] + HDy[Ny-1,Ny-2:Ny]
+A1[2*Ny+1, Ny+1:Ny+3] =     [dH[1],0] + HDy[1,1:3].todense()
+A1[3*Ny-1, 2*Ny-2:2*Ny] =   [0,dH[Ny-1]] + HDy[Ny-1,Ny-2:Ny].todense()
 A1[3*Ny,   2*Ny-1] =        HDy[Ny,Ny-1]
 
-for ii in xrange(1,Ny):
+for ii in range(1,Ny):
     A1[ii,ii]      = U[ii]
     A1[ii,Ny+ii]   = dU[ii] - f0
     A1[ii,2*Ny+ii] = g0
@@ -94,14 +100,14 @@ for ii in xrange(1,Ny):
 
     if ii == 1 or ii == (Ny-1):
         continue
-    A1[ii+2*Ny, Ny+ii-1:Ny+ii+2] = [0,dH[0,ii],0] + HDy[ii,ii-1:ii+2]
-sp.dia_matrix(A1)
+    A1[ii+2*Ny, Ny+ii-1:Ny+ii+2] = [0,dH[ii],0] + HDy[ii,ii-1:ii+2].todense()
 
-A2 = np.zeros([3*Ny+1, 3*Ny+1])
-for ii in xrange(1,Ny):
+
+# A2 = np.zeros([3*Ny+1, 3*Ny+1])
+A2 = sp.csr_matrix((3*Ny+1, 3*Ny+1))
+for ii in range(1,Ny):
     A2[Ny+ii,ii] = -f0
     A2[Ny+ii,2*Ny+ii-1:2*Ny+ii+2] = -g0*Dy[ii,ii-1:ii+2]
-sp.dia_matrix(A2)
 
 evalsArr = np.zeros(nk)
 cnt = 0
@@ -115,7 +121,7 @@ for kx in kk[0:nk]: #0:nk
     sp.dia_matrix(A)
 
     # Using eig
-    eigVals, eigVecs = spalg.eig(A)
+    eigVals, eigVecs = spalg.eig(A.todense())
     ind = (-np.imag(eigVals)).argsort() #get indices in descending order
     eigVecs = eigVecs[:,ind]
     eigVals = kx*eigVals[ind]
@@ -129,10 +135,10 @@ for kx in kk[0:nk]: #0:nk
     else: evals = len(eigVals)
     evalsArr[cnt] = evals
 
-    for i in xrange(evals):
+    for i in range(evals):
         grow[i,cnt] = np.imag(eigVals[i])*kx
         freq[i,cnt] = np.real(eigVals[i])*kx
-        for j in xrange(eigVals.shape[0]):
+        for j in range(eigVals.shape[0]):
             mode[j,i,cnt] = eigVecs[j,i]
     cnt = cnt+1
 
